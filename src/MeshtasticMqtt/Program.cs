a0b8cf178165ef353183210a60190a51454fe72b
boko <dcbokonon@gmail.com>
2025-04-22 10:25:56 -0400

    Reintegrate Serilog for broker logging and fix packet visibility

--- src/MeshtasticMqtt/Program.cs
+++ src/MeshtasticMqtt/Program.cs
@@ -3,17 +3,41 @@ namespace MeshtasticMqtt;
 using MeshtasticMqtt;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.DependencyInjection;
+using Serilog;
+using Serilog.Events;
+
 
 public class Program
 {
     public static async Task Main(string[] args)
     {
-        await Host.CreateDefaultBuilder(args)
-            .ConfigureServices((hostContext, services) =>
-            {
-                services.AddHostedService<MqttServerManager>();
-                services.AddSingleton<ClientDatabase>();
-            })
-            .RunConsoleAsync();
+        Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Debug()
+            .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+            .Enrich.FromLogContext()
+            .WriteTo.Console()
+            .CreateLogger();
+
+        try
+        {
+            Log.Information("Starting up");
+
+            await Host.CreateDefaultBuilder(args)
+                .UseSerilog()
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddHostedService<MqttServerManager>();
+                    services.AddSingleton<ClientDatabase>();
+                })
+                .RunConsoleAsync();
+        }
+        catch (Exception ex)
+        {
+            Log.Fatal(ex, "Host terminated unexpectedly");
+        }
+        finally
+        {
+            Log.CloseAndFlush();
+        }
     }
 }
